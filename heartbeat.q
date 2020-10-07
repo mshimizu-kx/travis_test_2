2068109f3debd85be8d95cb33ef8159060335d06
mshimizu-kx <mshimizu@glory.firstderivatives.com>
2020-10-08 00:26:35 +0900

    Check q is alive

---
+++ heartbeat.q
@@ -0,0 +1,3 @@
+i:10
+.z.ts:{-1 "alive..."; system "sleep 1"};
+\t 1000
--- travistest.sh
+++ travistest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup q -p 5000 < /dev/null >> qtest.log 2>&1 &
+nohup q heartbeat.q -p 5000 < /dev/null >> qtest.log 2>&1 &
 cargo run --example test --release
